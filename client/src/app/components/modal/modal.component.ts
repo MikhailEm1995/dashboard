a9857629eaf10443e3e6fd7310585d3a382967fd
Емченко Михаил <memchenko@htc-cs.ru>
2018-01-26 15:31:24 +0400

    Markup for store page is ready

--- client/src/app/components/modal/modal.component.ts
+++ client/src/app/components/modal/modal.component.ts
@@ -1,14 +1,48 @@
 import { Component, Input } from '@angular/core';
 
+class Comment {
+  avatar: string;
+  name: string;
+  time: string;
+  comment: string;
+  likes: string;
+}
+
+class AppData {
+  id: string;
+  name: string;
+  image: string;
+  rating: string;
+  category: string;
+  subCategory: string;
+  author: string;
+  version: string;
+  downloads: string;
+  eMail: string;
+  description: string;
+  previewImages: string[];
+  comments: Comment[];
+}
+
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
-  @Input() mainImage: string;
-  @Input() name: string;
-  @Input() rating: number;
-  @Input() description: string;
+  @Input() appData: AppData;
 
+  stars: string[];
+
+  getArrayOfStartsClasses() {
+    const result = [];
+    let rating = +parseFloat(this.appData.rating).toFixed();
+
+    for (let i = 0; i < 5; i += 1) {
+      result.push(rating > 0 ? 'modal__rating-star--filled' : 'modal__rating-star--empty');
+      if (rating > 0) rating -= 1;
+    }
+
+    return result;
+  }
 }
--- client/src/app/pages/store-page/store-page.component.ts
+++ client/src/app/pages/store-page/store-page.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-store-page',
   templateUrl: './store-page.component.html',
   styleUrls: ['./store-page.component.scss']
 })
-export class StorePageComponent implements OnInit {
+export class StorePageComponent {
 
   optionsOpened = false;
 
@@ -13,9 +13,39 @@ export class StorePageComponent implements OnInit {
     this.optionsOpened = !this.optionsOpened;
   }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  data = {
+    id: '1',
+    name: 'Accountant',
+    image: '/assets/images/mocks/mustache.jpg',
+    rating: '2.7',
+    category: 'management',
+    subCategory: 'finance',
+    author: 'Emchenko M',
+    version: '1.0.0',
+    downloads: '345',
+    eMail: 'emchenko.mikhail@mail.ru',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    previewImages: [
+      '/assets/images/mocks/mustache.jpg',
+      '/assets/images/mocks/mustache.jpg',
+      '/assets/images/mocks/mustache.jpg',
+      '/assets/images/mocks/mustache.jpg'
+    ],
+    comments: [
+      {
+        avatar: '/assets/images/mocks/mustache.jpg',
+        name: 'John Catson',
+        time: '15:35 12.05.2017',
+        comment: 'Lorem ipsum dolor sit amet!',
+        likes: '645'
+      },
+      {
+        avatar: '/assets/images/mocks/mustache.jpg',
+        name: 'Michael Kittison',
+        time: '16:35 12.05.2017',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        likes: '173'
+      }
+    ]
+  };
 }
